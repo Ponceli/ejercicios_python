641dfb1af3bceee96cceeaa0f9de33fee0ee0379
Eli Ponce <eli009423@gmail.com>
2024-10-30 13:43:25 -0400

    Este archivo es de python

---
+++ Ejercicio_aprendiendo_alf.py
@@ -0,0 +1,20 @@
+#Ejercicio 1
+#Escribir un programa que muestre por pantalla la cadena ¡Hola Mundo!.
+
+print("Hola Mundo!")
+
+#-----------------------------------------------
+
+var = ("Hola mundo")
+print(var)
+
+#-------------------------------------------------------
+name = input("Por favor ingrese su nombre: ")
+print("Hola", name)
+
+#---------------------------------------------
+
+#-----------------------------------------
+n = int(input("Por favor ingrese un numero: "))
+suma = n * (n + 1) / 2
+print ("La suma del numero" + (n) + "es:" + (suma))
